13b1e80b9db11a5f21db65b1c73d3538c2ac30d4
naveengitboi <naveenjangiti9@gmail.com>
2025-03-12 19:48:59 +0530

    added sorting and counting objs

--- testing.py
+++ testing.py
@@ -5,6 +5,7 @@ import cv2 as cv
 import serial
 import cvzone
 import math
+from sort import *
 
 
 url = "http://192.168.44.117:8080/video"
@@ -17,17 +18,20 @@ MAXSPEED = 3.0
 cap = cv.VideoCapture(1)
 cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
-# tracking and counting objects
 
 
 modelPath = "runs/detect/train3/weights/best.pt"
-
 model = YOLO(modelPath)
 
 counter = object_counter.ObjectCounter()
 coords = [topLeft,topRight, bottomRight, bottomLeft] = [(0,0), (1920,0), (1920,1080), (0,1080)]
 regionPoints = coords
-# counter.set_args(view_img = True, reg_pts = regionPoints, class_names = model.names, draw_tracks=True)
+
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+lx, ly, rx, ry = [450,152,600,152]
+lineCoords = [lx, ly, rx, ry]
+totalObjCounts = 0
 
 # linear interpolation
 def lerp(A, B , t):
@@ -49,40 +53,57 @@ while True:
     if not ret:
         print("Not connected")
         continue
-    if ret:
-        img = cv.flip(img, 1)
-        frame = img
-        coloredFrame = frame
+    img = cv.flip(img, 1)
+    frame = img
+    coloredFrame = frame
 
-        # gray color
-        grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # gray color
+    grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        # threshold Color
-        threshFrame = cv.threshold(grayFrame, 60, 255, cv.THRESH_BINARY)[1]
+    # threshold Color
+    threshFrame = cv.threshold(grayFrame, 60, 255, cv.THRESH_BINARY)[1]
 
-        results = model.predict(source=frame,stream=True, conf=0.05)
-        for result in results:
-            boxes = result.boxes
-            for box in boxes:
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                color = (255,0,255)
-                boundingBox = (x1, y1, x2-x1, y2-y1)
-                cvzone.cornerRect(frame, boundingBox, cv.LINE_AA)
+    results = model.predict(source=frame,stream=True, conf=0.05)
 
-                confidence = math.ceil((box.conf[0]*100))/100
-                positionOfText = (max(0, x1), max(35,y1))
+    detections = np.empty((0,5))
+    for result in results:
+        boxes = result.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            color = (255,0,255)
+            boundingBox = (x1, y1, x2-x1, y2-y1)
+            cvzone.cornerRect(frame, boundingBox, cv.LINE_AA)
 
-
-                cls = int(box.cls[0])
-                print(cls)
-                cvzone.putTextRect(frame, f'{classNames[cls]} {confidence}', positionOfText, scale=1.5, thickness=1)
+            confidence = math.ceil((box.conf[0]*100))/100
+            positionOfText = (max(0, x1), max(35,y1))
 
 
+            cls = int(box.cls[0])
+            cvzone.putTextRect(frame, f'{classNames[cls]} {confidence}', positionOfText,
+                               scale=1.5, thickness=1, offset=5)
+            currentArray = np.array([x1,y1,x2,y2, confidence])
+            detections = np.vstack([detections, currentArray])
 
-        cv.imshow("Results", frame)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+
+    trackerResults = tracker.update(detections)
+    cv.line(frame, (lineCoords[0], lineCoords[1]), (lineCoords[2], lineCoords[3]), (0,0,255), 2)
+    for tResult in trackerResults:
+        x1, y1, x2, y2, id = tResult
+        x1, y1, x2, y2, id = int(x1), int(y1), int(x2), int(y2), int(id)
+        w, h = x2-x1 , y2-y1
+        # cvzone.putTextRect()
+
+        cx, cy = x1 + w//2 , y1+ h//2
+
+        if lineCoords[0] < cx < lineCoords[2] and lineCoords[1] - 20 < cy < lineCoords[3] + 20:
+            totalObjCounts += 1
+
+    cvzone.putTextRect(frame, f'#of {totalObjCounts}',(50,50))
+
+    cv.imshow("Results", frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
 
 
 
